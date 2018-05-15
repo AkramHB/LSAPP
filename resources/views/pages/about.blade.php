4306acc6c6794dc18d5ee189fece69266c886523
Mahmoud <mahmoud@zalt.me>
2018-05-15 20:37:59 +0300

    everything changed

---
+++ resources/views/pages/about.blade.php
@@ -0,0 +1,19 @@
+<!doctype html>
+<html lang="{{ app()->getLocale() }}">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>{{config('app.name', 'LSAPP')}}</title>
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css">
+        <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"></script>
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script> 
+    </head>
+    <body>
+        <div class = "container bg-danger text-white">
+            <h3>About</h3>
+            <p>This is the About Page</p>
+        </div>
+    </body>
+</html>
